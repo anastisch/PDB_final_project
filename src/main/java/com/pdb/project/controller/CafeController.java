0886ec053d381e9f018cd76b561ed8559be6a292
kasim <kasimovara@mail.ru>
2022-05-15 15:21:55 +0300

    Add user functional

--- src/main/java/com/pdb/project/controller/CafeController.java
+++ src/main/java/com/pdb/project/controller/CafeController.java
@@ -1,19 +1,13 @@
 package com.pdb.project.controller;
 
-import com.pdb.project.model.Cafe;
-import com.pdb.project.model.User;
 import com.pdb.project.payload.request.CafeRequest;
-import com.pdb.project.payload.request.OwnershipRequest;
 import com.pdb.project.payload.response.MessageResponse;
-import com.pdb.project.payload.response.OwnershipClaimResponse;
-import com.pdb.project.security.service.UserDetailsImpl;
 import com.pdb.project.service.CafeService;
 import com.pdb.project.service.UserService;
 import com.pdb.project.service.YandexMapService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials = "true")
@@ -30,11 +24,29 @@ public class CafeController {
         this.mapService = mapService;
     }
 
+    @PostMapping
+    public void create(@RequestBody CafeRequest cafe) {
+        service.create(cafe);
+    }
+
     @PostMapping("/update")
-    public ResponseEntity<MessageResponse> updateCafeterias(@RequestParam(value = "res",defaultValue = "1") Integer res,
+    public ResponseEntity<MessageResponse> updateCafeterias(@RequestParam(value = "res", defaultValue = "1") Integer res,
                                                             @Value("${PDB.app.areas}") String areas,
                                                             @Value("${PDB.app.updateURL}") String urlPattern,
                                                             @Value("${PDB.app.mapAPI}") String apikey) {
         return mapService.updateCafeterias(res, areas, urlPattern, apikey);
+    }
+
+    @DeleteMapping("/{id}")
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void deleteCafe(@PathVariable long id) {
+        service.delete(id);
+    }
+
+    @PatchMapping("/{id}")
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void updateCafe(@PathVariable long id,
+                           @RequestBody CafeRequest cafe) {
+        service.update(id, cafe);
     }
 }
