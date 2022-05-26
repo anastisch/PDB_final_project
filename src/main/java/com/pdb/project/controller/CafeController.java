eea89fd22762d52421b46b7075811669696bfd0a
bubulkka <anastisch@gmail.com>
2022-05-26 10:31:24 +0300

    Add review functionality and getting coffee shop pages

--- src/main/java/com/pdb/project/controller/CafeController.java
+++ src/main/java/com/pdb/project/controller/CafeController.java
@@ -1,13 +1,22 @@
 package com.pdb.project.controller;
 
+import com.pdb.project.model.Cafe;
+import com.pdb.project.model.User;
 import com.pdb.project.payload.request.CafeRequest;
+import com.pdb.project.payload.request.GradeRequest;
 import com.pdb.project.payload.response.MessageResponse;
+import com.pdb.project.security.service.UserDetailsImpl;
 import com.pdb.project.service.CafeService;
 import com.pdb.project.service.UserService;
 import com.pdb.project.service.YandexMapService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials = "true")
@@ -37,6 +46,27 @@ public class CafeController {
         return mapService.updateCafeterias(res, areas, urlPattern, apikey);
     }
 
+    @GetMapping
+    public List<Cafe> getCafePage(@RequestParam(value = "page") Integer page,
+                                  @RequestParam(value = "location", defaultValue = "59.965361,30.311645") String location,
+                                  @RequestParam(value = "dist", defaultValue = "1.0") Double dist,
+                                  @RequestParam(value = "confirmed", defaultValue = "true") boolean confirmed) {
+        Page<Cafe> cafeterias = service.getPage(page, location, dist, confirmed);
+        return cafeterias.getContent();
+    }
+
+    @GetMapping("/pages-count")
+    public int getCountPage(@RequestParam(value = "location", defaultValue = "59.965361,30.311645") String location,
+                            @RequestParam(value = "dist", defaultValue = "1.0") Double dist,
+                            @RequestParam(value = "confirmed", defaultValue = "true") boolean confirmed) {
+        return service.getPageCount(location, dist, confirmed);
+    }
+
+    @GetMapping("/{id}")
+    public Cafe getCafe(@PathVariable long id) {
+        return service.getById(id);
+    }
+
     @DeleteMapping("/{id}")
     //@PreAuthorize("hasRole('ROLE_ADMIN')")
     public void deleteCafe(@PathVariable long id) {
@@ -48,5 +78,30 @@ public class CafeController {
     public void updateCafe(@PathVariable long id,
                            @RequestBody CafeRequest cafe) {
         service.update(id, cafe);
+    }
+
+    @PostMapping("/{id}/review")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MODERATOR', 'ROLE_USER')")
+    public ResponseEntity<MessageResponse> addReview(Authentication auth,
+                                                     @PathVariable long id,
+                                                     @RequestBody GradeRequest grade) {
+        UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
+        User user = userService.getById(userDetails.getId());
+        return service.addReview(id, user, grade);
+    }
+
+    @PatchMapping("/{id}/review/{userId}")
+    @PreAuthorize("#userId == authentication.principal.id or hasRole('ROLE_ADMIN')")
+    public ResponseEntity<MessageResponse> updateReview(@PathVariable long id,
+                                                        @PathVariable long userId,
+                                                        @RequestBody GradeRequest grade) {
+        return service.updateReview(id, userId, grade);
+    }
+
+    @DeleteMapping("/{id}/review/{userId}")
+    @PreAuthorize("#userId == authentication.principal.id or hasRole('ROLE_ADMIN')")
+    public ResponseEntity<MessageResponse> deleteReview(@PathVariable long id,
+                                                        @PathVariable long userId) {
+        return service.deleteReview(id, userId);
     }
 }
--- src/main/java/com/pdb/project/repository/CafeRepository.java
+++ src/main/java/com/pdb/project/repository/CafeRepository.java
@@ -1,7 +1,10 @@
 package com.pdb.project.repository;
 
 import com.pdb.project.model.Cafe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +14,34 @@ public interface CafeRepository extends JpaRepository<Cafe, Long> {
     Optional<Cafe> findById(Long id);
 
     boolean existsByIdApi(Long idApi);
+
+    @Query(value =
+            "select * from cafeterias " +
+                    "where point_id in (" +
+                    "        select id from points" +
+                    "        where 111.2 * |/( (:lat - lat)^2 + ((:lng - lng)*cos(pi()*:lat/180))^2 ) <= :dist" +
+                    "    )",
+            nativeQuery = true)
+    Page<Cafe> findNearbyCoffeeShops(Double lat, Double lng, Double dist, Pageable pageable);
+
+    @Query(value =
+            "select count(*) from cafeterias " +
+                    "where point_id in (" +
+                    "        select id from points" +
+                    "        where 111.2 * |/( (:lat - lat)^2 + ((:lng - lng)*cos(pi()*:lat/180))^2 ) <= :dist" +
+                    "    )",
+            nativeQuery = true)
+    int countNearbyCoffeeShops(Double lat, Double lng, Double dist);
+
+    @Query(value =
+            "select * from cafeterias " +
+                    "where confirmed = false",
+            nativeQuery = true)
+    Page<Cafe> findUnconfirmedCoffeeShops(Pageable pageable);
+
+    @Query(value =
+            "select count(*) from cafeterias " +
+                    "where confirmed = false",
+            nativeQuery = true)
+    int countUnconfirmedCoffeeShops();
 }
